cf604bbfe058dc88ac1bef9779e0ca57c6fec1ad
H-Blues <hblues419@gmail.com>
2023-05-25 21:49:56 +0800

    feat: add routes and some configs in index file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Web3ReactProvider } from "@web3-react/core";
+import { ethers } from "ethers";
+import Home from "./pages/Home";
+import Header from "./layouts/Header";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const getLibrary = (provider) => {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 8000; // frequency provider is polling
+  return library;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Web3ReactProvider>
+  );
+};
+
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(<App />);
