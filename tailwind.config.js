77f53179d342a38f4169566bcbc76f17a5cd0a4c
H-Blues <hblues419@gmail.com>
2023-05-26 20:57:48 +0800

    chore: add tailwind-css, web3-react and some other dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["SFMono-Regular", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+});
