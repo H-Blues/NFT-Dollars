36fcb0cd1db3a51585d3033025beb9b2d3fa0cec
H-Blues <hblues419@gmail.com>
2023-06-07 18:27:44 +0800

    style: change the font color based on the validity of the input.

--- src/components/input/usdInput.js
+++ src/components/input/usdInput.js
@@ -2,11 +2,32 @@ import React, { useState } from "react";
 import { Button, Input, Typography } from "@material-tailwind/react";
 import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
-const NFTInput = (props) => {
-  const [inputValue, setInputValue] = useState("");
+const USDInput = (props) => {
+  const [inputValue, setInputValue] = useState(0.0);
+  const [exceedMax, setExceedMax] = useState(false);
+
   const handleChange = (event) => {
-    setInputValue(event.target.value);
-    props.inputValueChange(event.target.value);
+    const inputValue = event.target.value;
+
+    if (parseFloat(inputValue) >= 0) {
+      setInputValue(inputValue);
+      props.inputValueChange(inputValue);
+    } else if (inputValue == "") {
+      setInputValue("");
+      props.inputValueChange("");
+    }
+
+    if (parseFloat(inputValue) > props.maxValue) {
+      setExceedMax(true);
+    } else {
+      setExceedMax(false);
+    }
+  };
+
+  const setMaxValue = () => {
+    setInputValue(props.maxValue);
+    props.inputValueChange(props.maxValue);
+    setExceedMax(false);
   };
 
   return (
@@ -18,19 +39,28 @@ const NFTInput = (props) => {
           value={inputValue}
           label={props.title}
           onChange={handleChange}
+          error={exceedMax}
         />
-        {!props.isMax && (
-          <Button color="orange" size="sm" className="!absolute right-1 top-1 rounded">
-            max
-          </Button>
-        )}
+        <Button
+          color="orange"
+          size="sm"
+          className="!absolute right-1 top-1 rounded"
+          onClick={setMaxValue}
+        >
+          max
+        </Button>
       </div>
-      <Typography variant="small" color="gray" className="flex items-center gap-1 font-normal mt-2">
+      <Typography
+        variant="small"
+        className={`flex items-center gap-1 font-normal mt-2 ${
+          exceedMax ? "text-red-500" : "text-gray-700"
+        }`}
+      >
         <InformationCircleIcon className="w-4 h-4 -mt-px" />
-        {props.tip}
+        {!exceedMax ? props.tip : "Your input exceeds the maximum extraction "}
       </Typography>
     </div>
   );
 };
 
-export default NFTInput;
+export default USDInput;
